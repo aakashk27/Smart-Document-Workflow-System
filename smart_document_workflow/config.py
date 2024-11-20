7ae5554e71322311b37d93c168582d405be564f1
Aakash Khanna <aakash27khana@gmail.com>
2024-11-20 22:36:17 +0530

    Removed DB url

--- smart_document_workflow/config.py
+++ smart_document_workflow/config.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+    MONGO_URI = os.getenv("MONGO_URI", "default")
+
+    if(MONGO_URI.startswith("default")):
+        raise ValueError("MONGO_URI not set in .env file")
